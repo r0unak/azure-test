0c7d87f785ad7f92c635655b281cdbae31ee54da
Vijay Patel <35554905+easyawslearn@users.noreply.github.com>
2024-07-18 13:38:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,15 @@ provider "azurerm" {
   }
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-stage-azure-open-ai"
+    storage_account_name = "terraformstateopen2024ai"
+    container_name       = "terraformopenai2024"
+    key                  = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources5"
   location = "West Europe"
