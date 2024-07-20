68abde09edeed8d16febe031aeb84822b85efea8
Vijay <s3cloudhub@gamil.com>
2024-07-20 10:57:51 +0530

    test

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "azurerm" {
   }
 }
 
+/*
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-stage-azure-open-ai"
@@ -15,6 +16,7 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+*/
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources5"
